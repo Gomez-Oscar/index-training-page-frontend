564185efd2fdea4dbc5bf66cf46a00469326d229
Gomez-Oscar <alexgomez212121@gmail.com>
2021-10-26 23:01:20 -0500

    search bar style was improved

--- src/App.jsx
+++ src/App.jsx
@@ -17,21 +17,23 @@ const App = () => {
       <MainHeader />
       <main>
         <Switch>
-          <h1 className="main-title">Index Training Resources</h1>
-          <Searchbar />
-          <h2 className="secondary-title">Slides</h2>
+          <Route to="/" exact>
+            <h1 className="main-title">Index Training Resources</h1>
+            <Searchbar />
+            <h2 className="secondary-title">Slides</h2>
 
-          <div className="wrapper">
-            <Card title="Talent Tools" />
-            <Card title="Tickets System" />
-            <Card title="Pagination" />
-          </div>
-          <h2 className="secondary-title">Videos</h2>
-          <div className="wrapper">
-            <Card title="Scroll Training" />
-            <Card title="XML Training" />
-            <Card title="Button More Training" />
-          </div>
+            <div className="wrapper">
+              <Card title="Talent Tools" />
+              <Card title="Tickets System" />
+              <Card title="Pagination" />
+            </div>
+            <h2 className="secondary-title">Videos</h2>
+            <div className="wrapper">
+              <Card title="Scroll Training" />
+              <Card title="XML Training" />
+              <Card title="Button More Training" />
+            </div>
+          </Route>
           <Redirect to="/" />
         </Switch>
       </main>
--- src/homepage/components/SearchBar.jsx
+++ src/homepage/components/SearchBar.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 import './SearchBar.css';
 
 const Searchbar = () => {
   return (
-    <div className="search-bar-container">
+    /*     <div className="search-bar-container">
       <input type="text" placeholder="xml training" className="search-bar" />
+      <button className="search-button"></button>
+    </div> */
+    <div className="search-wrap">
+      <div className="search-box">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="xml training"
+        />
+        <button className="search-btn">
+          <FaSearch size="25px" />
+        </button>
+      </div>
     </div>
   );
 };
