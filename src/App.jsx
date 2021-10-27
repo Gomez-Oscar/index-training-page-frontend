f499ce82b6225420780e87a946419d62797cd512
Gomez-Oscar <alexgomez212121@gmail.com>
2021-10-26 19:01:12 -0500

    se agrega el paquete react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import Card from './shared/components/UIElements/Card';
 import Searchbar from './homepage/components/SearchBar';
@@ -7,25 +13,29 @@ import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
+    <Router>
       <MainHeader />
       <main>
-        <h1 className="main-title">Index Training Resources</h1>
-        <Searchbar />
-        <h2 className="secondary-title">Slides</h2>
-        <div className="wrapper">
-          <Card title="Talent Tools" />
-          <Card title="Tickets System" />
-          <Card title="Pagination" />
-        </div>
-        <h2 className="secondary-title">Videos</h2>
-        <div className="wrapper">
-          <Card title="Scroll Training" />
-          <Card title="XML Training" />
-          <Card title="Button More Training" />
-        </div>
+        <Switch>
+          <h1 className="main-title">Index Training Resources</h1>
+          <Searchbar />
+          <h2 className="secondary-title">Slides</h2>
+
+          <div className="wrapper">
+            <Card title="Talent Tools" />
+            <Card title="Tickets System" />
+            <Card title="Pagination" />
+          </div>
+          <h2 className="secondary-title">Videos</h2>
+          <div className="wrapper">
+            <Card title="Scroll Training" />
+            <Card title="XML Training" />
+            <Card title="Button More Training" />
+          </div>
+          <Redirect to="/" />
+        </Switch>
       </main>
-    </Fragment>
+    </Router>
   );
 };
 
--- src/shared/components/Navigation/MainHeader.jsx
+++ src/shared/components/Navigation/MainHeader.jsx
@@ -5,7 +5,7 @@ import './MainHeader.css';
 const MainHeader = (props) => {
   return (
     <header className="main-header">
-      Index Training Resources
+      Index Training
       {props.children}
     </header>
   );
